d36af7ee7a7674e908dfda460d2aff546d46a5ff
ameeraazli <ameera.azli@ytl.com>
2023-05-23 14:28:32 +0800

    configuring the drawer navigator - specific to each screen

--- App.js
+++ App.js
@@ -10,7 +10,17 @@ export default function App() {
   return (
     <NavigationContainer>
       <Drawer.Navigator initialRouteName='User'>
-        <Drawer.Screen name="Welcome" component={WelcomeScreen} />
+        <Drawer.Screen
+          name="Welcome"
+          component={WelcomeScreen}
+          options={{
+            headerStyle: {backgroundColor: '#3c0a6b'},
+            headerTintColor: 'white',
+            drawerLabel: 'Welcome Screen',
+            drawerActiveBackgroundColor: '#f0e1ff',
+            drawerActiveTintColor: '#3c0a6b',
+            drawerStyle: {backgroundColor: '#ccc'}
+        }} />
         <Drawer.Screen name="User" component={UserScreen} />
       </Drawer.Navigator>
     </NavigationContainer>
