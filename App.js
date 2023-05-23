115be3edfa2932246f018a57e58f591860b526fa
ameeraazli <ameera.azli@ytl.com>
2023-05-23 15:14:57 +0800

    change to use BottomTabNavigator instead - including ionicons

--- App.js
+++ App.js
@@ -16,20 +16,31 @@ export default function App() {
       <BottomTab.Navigator screenOptions={{
         headerStyle: {backgroundColor: '#3c0a6b'},
         headerTintColor: 'white',
-
       }}>
         <BottomTab.Screen
           name="Welcome"
           component={WelcomeScreen}
           options={{
-
+            tabBarIcon: ({color, size}) => (
+              <Ionicons
+                name="home"
+                color={color}
+                size={size}
+              />
+            ),
           }}
         />
         <BottomTab.Screen
           name="User"
           component={UserScreen}
           options={{
-
+            tabBarIcon: ({color, size}) => (
+              <Ionicons
+                name="person"
+                color={color}
+                size={size}
+              />
+            ),
           }}
         />
       </BottomTab.Navigator>
