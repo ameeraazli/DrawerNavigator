94410933968c12fa4bf555e5876a084caf8c47d3
ameeraazli <ameera.azli@ytl.com>
2023-05-23 14:40:59 +0800

    configuring the drawer navigator - using ionicons

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Ionicons } from '@expo/vector-icons';
 
 import WelcomeScreen from './screens/WelcomeScreen';
 import UserScreen from './screens/UserScreen';
@@ -19,9 +20,21 @@ export default function App() {
           name="Welcome"
           component={WelcomeScreen}
           options={{
-            drawerLabel: 'Welcome Screen'
-        }} />
-        <Drawer.Screen name="User" component={UserScreen} />
+            drawerLabel: 'Welcome Screen',
+            drawerIcon: ({color, size}) => (
+              <Ionicons name="home" color={color} size={size} />
+            ),
+          }}
+        />
+        <Drawer.Screen
+          name="User"
+          component={UserScreen}
+          options={{
+            drawerIcon: ({color, size}) => (
+              <Ionicons name="person" color={color} size={size}/>
+            ),
+          }}
+        />
       </Drawer.Navigator>
     </NavigationContainer>
   );
